716842e748c428c86c9e3c2cb64dcd2fa2bf52b6
Arkadiusz Rypulak <83660682+arkadiuszrypulak@users.noreply.github.com>
2023-06-01 09:33:05 +0200

    możliwość dodawania produktu do koszyka

--- src/components/PriceFooter.tsx
+++ src/components/PriceFooter.tsx
@@ -1,4 +1,5 @@
-// import React from 'react'
+import React, {useEffect, useState} from 'react'
+import useShoppingCart from '../hooks/useShoppingCart'
 import { CreateTermoblockItemInput, plHinges, plColors } from '../schema/termoblockItem.schema'
 type PriceFooterProps = {
   termoblock: CreateTermoblockItemInput,
@@ -6,7 +7,10 @@ type PriceFooterProps = {
 }
 
 function PriceFooter(props: PriceFooterProps) {
-  const termoblock = props.termoblock;
+  const [getItems, addItem, removeItem, getSum, changeQuantity] =
+  useShoppingCart();
+
+  const [termoblock, setTermoblock] = useState(props.termoblock);
   const isValid = props.isValid;
 
   const checkNan = (value:number) =>  {
@@ -16,51 +20,56 @@ function PriceFooter(props: PriceFooterProps) {
     return value;
   }
 
+  useEffect(() => {
+    setTermoblock(props.termoblock);
+  }, [props.termoblock]);
+console.log(termoblock,termoblock.color)
+
   return (
      <div className='hidden md:block fixed bottom-0 rounded-t-lg left-1/2 -translate-x-1/2 w-2/3 bg-white min-h-[36px] h-auto shadow shadow-mainOrange'>
        <div className='m-4 flex md:flex-2 flex-wrap justify-center'>
          <div className='w-1/2 md:w-1/3 lg:w-1/4 mb-2 text-center flex-col'>
            <p>Rozmiar:</p>
-           <p className='text-mainOrange text-lg'> A: {checkNan(termoblock.width)}mm, B: {checkNan(termoblock.height)}mm</p>
+           <p className='text-mainOrange text-lg font-bold'> A: {checkNan(termoblock.width)}mm, B: {checkNan(termoblock.height)}mm</p>
          </div>
 
          {termoblock.felc === undefined || isNaN(termoblock.felc) ? null :  <div className='w-1/2 md:w-1/3 lg:w-1/4 mb-2 text-center flex flex-col'>
            <p>Felc:</p>
-           <p className='text-mainOrange text-lg'>{termoblock.felc}mm</p>
+           <p className='text-mainOrange text-lg font-bold'>{termoblock.felc}mm</p>
          </div>}
 
          <div className='w-1/2 md:w-1/3 lg:w-1/4 mb-2 text-center flex flex-col'>
            <p>Kolor:</p>
-           <p className='text-mainOrange text-lg'>{termoblock.color ? plColors[termoblock.color] : '-'}</p>
+           <p className='text-mainOrange text-lg font-bold'>{termoblock.color ? plColors[termoblock.color] : '-'}</p>
          </div>
 
          <div className='w-1/2 md:w-1/3 lg:w-1/4 mb-2 text-center flex-col'>
            <p>Zawias:</p>
-           <p className='text-mainOrange text-lg'>{termoblock.hinges ? plHinges[termoblock.hinges] : '-'}</p>
+           <p className='text-mainOrange text-lg font-bold'>{termoblock.hinges ? plHinges[termoblock.hinges] : '-'}</p>
          </div>
 
           {termoblock.firstHole ? <div className='w-1/2 md:w-1/3 lg:w-1/4 mb-2 text-left flex-col'>
            <p>Rodzaj pierwszego otworu:</p>
-           {termoblock.firstHole?.holeType === 'okrągły na rurę bez uchwytu' ? <p className='text-mainOrange text-lg'>{termoblock.firstHole.diameter !== undefined && checkNan(termoblock.firstHole.diameter)}mm</p> : null }
-           <p className='text-mainOrange text-lg'>{termoblock.firstHole?.holeType}</p>
-          <p className='text-mainOrange text-lg'>{termoblock.firstHole?.stringPosition}</p>
+           {termoblock.firstHole?.holeType === 'okrągły na rurę bez uchwytu' ? <p className='text-mainOrange text-lg font-bold'>{termoblock.firstHole.diameter !== undefined && checkNan(termoblock.firstHole.diameter)}mm</p> : null }
+           <p className='text-mainOrange text-lg font-bold'>{termoblock.firstHole?.holeType}</p>
+          <p className='text-mainOrange text-lg font-bold'>{termoblock.firstHole?.stringPosition}</p>
          </div> : null}
 
          {termoblock?.hasSecondHole && termoblock.secondHole ? 
          (<div className='w-1/2 md:w-1/3 lg:w-1/4 my-2 text-left flex-col'>
            <p>Rodzaj drugiego otworu:</p>
            {termoblock.secondHole?.holeType === 'okrągły na rurę bez uchwytu' ? 
-           <p className='text-mainOrange text-lg'>{termoblock.secondHole.diameter !== undefined && checkNan(termoblock.secondHole.diameter)}mm</p> 
+           <p className='text-mainOrange text-lg font-bold'>{termoblock.secondHole.diameter !== undefined && checkNan(termoblock.secondHole.diameter)}mm</p> 
            : null }
-           <p className='text-mainOrange text-lg'>{termoblock.secondHole?.holeType}</p>
-           <p className='text-mainOrange text-lg'>{termoblock.secondHole?.stringPosition}</p>
+           <p className='text-mainOrange text-lg font-bold'>{termoblock.secondHole?.holeType || 1}</p>
+           <p className='text-mainOrange text-lg font-bold'>{termoblock.secondHole?.stringPosition}</p>
          </div> )
          : null} 
 
          {termoblock.hasPowerCordHole && termoblock.powerCordHole ? 
          (<div className='w-1/2 md:w-1/3 lg:w-1/4 my-2 text-left flex-col'>
            <p>Otwór na przewód zasilający:</p>
-           <p className='text-mainOrange text-lg'>{termoblock.powerCordHole.stringPosition}</p>
+           <p className='text-mainOrange text-lg font-bold'>{termoblock.powerCordHole.stringPosition}</p>
          </div>) 
          : null} 
        </div>
@@ -70,8 +79,32 @@ function PriceFooter(props: PriceFooterProps) {
          (<div>
            <span className='border border-mainOrange inline-block px-6 py-4'>Wpisz w formularzu parametry, po wpisaniu pojawi się cena produktu</span>
          </div>) : 
-         (<div>
-          cena
+         (<div className='flex items-center justify-center w-full'>
+            <p className='text-2xl'><span className='text-4xl font-bold text-mainOrange'>55,00</span> ZŁ</p>
+            <button onClick={() =>
+        addItem({
+          id: 1,
+          name: "Termoblock",
+          price: 55,
+          quantity: 1,
+          details: {
+            width: termoblock.width,
+            height: termoblock.height,
+            color: termoblock.color,
+            felc: termoblock.felc,
+            hinges: termoblock.hinges,
+            firstHole: {
+              stringPosition: termoblock.firstHole.stringPosition,
+              holeType: termoblock.firstHole.holeType,
+            },
+            hasSecondHole: false,
+            hasPowerCordHole: false,
+          },
+        })
+      }> 
+          <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" className='m-4 border border-mainOrange rounded-full bg-mainOrange text-white w-8 animate-bounce'>
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15"></path>
+          </svg></button>
          </div>)}
        </div>
      </div>
