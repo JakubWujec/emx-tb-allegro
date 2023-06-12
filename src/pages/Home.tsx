afcb7c2b05f31eaa6392a82f56061b7598881e7f
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2023-06-12 08:47:29 +0200

    home navigation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,24 @@
+import { Link } from "react-router-dom";
 import ConfiguratorPro from "./ConfiguratorPro";
 
 const Home = () => {
   return (
     <div className="relative">
-      <ConfiguratorPro />
+      <div>
+        <Link className="text-black-500" to={`/up`}>
+          UP
+        </Link>
+      </div>
+      <div>
+        <Link className="text-black-500" to={`/go`}>
+          GO
+        </Link>
+      </div>
+      <div>
+        <Link className="text-black-500" to={`/pro`}>
+          PRO
+        </Link>
+      </div>
     </div>
   );
 };
