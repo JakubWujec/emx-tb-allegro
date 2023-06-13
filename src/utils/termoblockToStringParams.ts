b44936043c62ef1d06799c5fd5b29b253f430065
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2023-06-13 08:25:17 +0200

    stringParams

--- src/utils/termoblockToStringParams.ts
+++ src/utils/termoblockToStringParams.ts
@@ -1,4 +1,4 @@
-import { StringParam, TermoblockItem } from "../types";
+import { StringParam, TermoblockHole, TermoblockItem } from "../types";
 
 const termoblockToStringParams = (
   termoblock: TermoblockItem
@@ -6,23 +6,69 @@ const termoblockToStringParams = (
   const result: StringParam[] = [];
 
   result.push({
-    label: "Rozmiar",
-    value: `${termoblock.width} x ${termoblock.height}`,
+    label: "Nazwa",
+    value: `${termoblock.name}`,
+  });
+
+  result.push({
+    label: "Szerokość",
+    value: `${termoblock.width} (mm)`,
+  });
+
+  result.push({
+    label: "Wysokość",
+    value: `${termoblock.height} (mm)`,
   });
 
   if ("color" in termoblock) {
     result.push({
       label: "Kolor",
-      value: `${termoblock.color}`,
+      value: termoblock.color,
+    });
+  }
+
+  if ("felc" in termoblock) {
+    result.push({
+      label: "Felc",
+      value: termoblock.felc.toString(),
     });
   }
 
   if ("hinges" in termoblock) {
     result.push({
       label: "Zawiasy",
-      value: `${termoblock.hinges}`,
+      value: termoblock.hinges,
+    });
+  }
+
+  result.push({
+    label: "Pierwszy otwór",
+    value: holeStringParamValue(termoblock.firstHole),
+  });
+
+  if ("secondHole" in termoblock) {
+    result.push({
+      label: "Drugi otwór",
+      value: holeStringParamValue(termoblock.secondHole),
+    });
+  }
+
+  if ("thirdHole" in termoblock) {
+    result.push({
+      label: "Trzeci otwór",
+      value: holeStringParamValue(termoblock.thirdHole),
     });
   }
 
   return result;
 };
+
+function holeStringParamValue(hole: TermoblockHole) {
+  let result = `${hole.holeType}, ${hole.stringPosition}`;
+  if (hole.diameter) {
+    result = result.concat(`, ${hole.diameter}`);
+  }
+  return result;
+}
+
+export default termoblockToStringParams;
