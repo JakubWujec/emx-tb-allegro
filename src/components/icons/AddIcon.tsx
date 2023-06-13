82ec9e96f4e7d861d63f53e3b977450b85b50f6d
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2023-06-13 09:46:25 +0200

    hover zamiast bounce w addIcon

--- src/components/icons/AddIcon.tsx
+++ src/components/icons/AddIcon.tsx
@@ -7,7 +7,7 @@ const AddIcon = () => {
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
       aria-hidden="true"
-      className="m-4 border border-mainOrange rounded-full bg-mainOrange text-white w-8 animate-bounce"
+      className="m-4 border border-mainOrange rounded-full bg-mainOrange text-white w-8 hover:bg-mainOrangeDarker"
     >
       <path
         strokeLinecap="round"
